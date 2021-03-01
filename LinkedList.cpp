a011ff9010c0b150be0d85be43fdbe45f0886734
vicktorhu <vicktor.hutama@gmail.com>
2021-03-01 17:38:19 +0700

    Initial Commit

---
+++ LinkedList.cpp
@@ -0,0 +1,77 @@
+//
+// Created by Vic on 3/1/2021.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Node {
+private:
+    int value;
+    Node *next = NULL;
+public:
+    Node(int val) {
+        value = val;
+    }
+
+    int getValue() const {
+        return value;
+    }
+
+    void setValue(int value) {
+        Node::value = value;
+    }
+
+    Node *getNext() const {
+        return next;
+    }
+
+    void setNext(Node *next) {
+        Node::next = next;
+    }
+};
+
+class LinkedList {
+private:
+    Node *root;
+
+    void add(int value, Node *currentNode) {
+        if (currentNode->getNext() == NULL) {
+            currentNode->setNext(new Node(value));
+        } else {
+            add(value, currentNode->getNext());
+        }
+    }
+
+    void printAll(Node *currentNode) {
+        cout << "(" << currentNode->getValue() << ")";
+        if (currentNode->getNext() != NULL) {
+            cout << "->";
+            printAll(currentNode->getNext());
+        }
+    }
+
+public:
+    LinkedList(int rootValue) {
+        root = new Node(rootValue);
+    }
+
+    void add(int value) {
+        add(value, root);
+    }
+
+    void printAll() {
+        printAll(root);
+    }
+};
+
+int main() {
+    LinkedList linkedList(5);
+    linkedList.add(7);
+    linkedList.add(9);
+    linkedList.add(12);
+    linkedList.add(19);
+    linkedList.printAll();
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Compilation of basic data structure classes made with C++";
+    return 0;
+}
