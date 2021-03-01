da41f4d9b49428b8d2ff4b31e24bf2032e34ce9f
vicktorhu <vicktor.hutama@gmail.com>
2021-03-01 19:58:35 +0700

    Added set(), get() and insert()

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -52,6 +52,32 @@ private:
         }
     }
 
+    Node *get(int index, Node *currentNode) {
+        if (index == 0) {
+            return currentNode;
+        } else {
+            return get(index - 1, currentNode->getNext());
+        }
+    }
+
+    void set(int index, int value, Node *currentNode) {
+        if (index == 0) {
+            currentNode->setValue(value);
+        } else {
+            set(index - 1, value, currentNode->getNext());
+        }
+    }
+
+    void insert(int index, int value, Node *currentNode) {
+        if (index == 1) {
+            Node *newNode = new Node(value);
+            newNode->setNext(currentNode->getNext());
+            currentNode->setNext(newNode);
+        } else {
+            insert(index - 1, value, currentNode->getNext());
+        }
+    }
+
 public:
     LinkedList(int rootValue) {
         root = new Node(rootValue);
@@ -64,14 +90,42 @@ public:
     void printAll() {
         printAll(root);
     }
+
+    Node *get(int index) {
+        return get(index, root);
+    }
+
+    void set(int index, int value) {
+        set(index, value, root);
+    }
+
+    void insert(int index, int value) {
+        insert(index, value, root);
+    }
 };
 
 int main() {
+    // init
     LinkedList linkedList(5);
     linkedList.add(7);
     linkedList.add(9);
     linkedList.add(12);
     linkedList.add(19);
     linkedList.printAll();
+
+    // get(2)
+    cout << "\nGet 2nd item: " << linkedList.get(2)->getValue();
+
+    // set(2, 19)
+    cout << "\nset(2, 19)\n";
+    linkedList.set(2, 19);
+    linkedList.printAll();
+
+    // insert(3, 15)
+    cout << "\ninsert(3, 15)\n";
+    linkedList.insert(3, 15);
+    linkedList.printAll();
+
+
     return 0;
 }
